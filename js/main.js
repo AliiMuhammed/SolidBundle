638a4ad148822e85c8dca38828a3546d631918df
AliiMuhammed <mohamedtaha8000@gmail.com>
2023-07-12 17:05:48 +0300

    about  us section with responsive

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
+
+
+//----------------------------- start header --------------------------------------------
+
 const navLinks = document.querySelectorAll('.nav-link');
 const toggleBtn = document.querySelector('.toggle-menu-btn');
 const links = document.querySelector('.links');
-
-
-
+// open toggle menu when click on toggle btn 
 toggleBtn.addEventListener('click', function() {
   this.classList.toggle('active');
   links.classList.toggle('active-menu');
 });
+
+// active link color when click on it 
 navLinks.forEach(link => {
   link.addEventListener('click', function() {
     navLinks.forEach(link => link.classList.remove('active'));
@@ -16,3 +20,7 @@ navLinks.forEach(link => {
     toggleBtn.classList.remove('active');
   });
 });
+
+//----------------------------- end header --------------------------------------------
+
+
