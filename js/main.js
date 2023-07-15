12534bb01a4e375cea627b8e7959e87fca1e91d7
AliiMuhammed <mohamedtaha8000@gmail.com>
2023-07-15 07:06:51 +0300

    last update
    scroll to the exact top of a specific section when click on any link in nav bar
    
    fix the color of the gap between the footer and contact section

--- js/main.js
+++ js/main.js
@@ -50,6 +50,23 @@ links.forEach(function(link) {
     
     document.addEventListener('scroll', updateActiveLink);
 
+// To scroll to the exact top of a specific section
+    document.addEventListener("DOMContentLoaded", function() {
+      const navLinks = document.querySelectorAll(".nav-link");
+      navLinks.forEach(function(link) {
+        link.addEventListener("click", function(event) {
+          event.preventDefault();
+          const targetId = link.getAttribute("href").substring(1);
+          const targetSection = document.getElementById(targetId);
+          const targetPosition = targetSection.offsetTop - document.querySelector("header").offsetHeight;
+          window.scrollTo({
+            top: targetPosition,
+            behavior: "smooth"
+          });
+        });
+      });
+    });
+    
 //----------------------------- end header --------------------------------------------
 
 
@@ -144,3 +161,8 @@ btn.onclick = () => {
     behavior: "smooth",
   });
 };
+
+
+
+
+
